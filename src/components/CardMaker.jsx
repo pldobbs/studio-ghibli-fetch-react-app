2b697a4281509bd96b3dbfee20249cfbb4569505
Paul <pldobbs@mac.com>
2018-01-24 23:01:56 -0600

    added react router

--- src/components/CardMaker.jsx
+++ src/components/CardMaker.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class CardMaker extends Component {
     render() {
+        console.log(this.props.film);
         let film = this.props.film.map((film) => {
             return (
                 <div className="card col-6" style={{ width: '20rem' }} key={film.id}>
